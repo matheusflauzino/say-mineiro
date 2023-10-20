a8ac19982db40b1aa8d54851295bfa2425903c03
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2023-10-19 21:32:57 -0300

    feat: configure modules

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+export declare function all(): any;
+export declare function random(): any;
+declare const _default: {
+    all: typeof all;
+    random: typeof random;
+};
+export default _default;
