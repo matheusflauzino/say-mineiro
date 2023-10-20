6ec9c04bebf2eab5ca079434f4cb728f7580717f
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2023-10-19 21:36:19 -0300

    refactor: remove file

--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function all(): any;
-export declare function random(): any;
-declare const _default: {
-    all: typeof all;
-    random: typeof random;
-};
-export default _default;
